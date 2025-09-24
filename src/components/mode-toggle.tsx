fbf7f259a089462ff0d8f5cb92806c654a8f52bc
rizkirifandi7 <rizkirifandi7@gmail.com>
2025-09-24 21:47:56 +0700

    button dark mode

--- src/components/mode-toggle.tsx
+++ src/components/mode-toggle.tsx
@@ -4,6 +4,6 @@ import { AnimatedThemeToggler } from "./ui/animated-theme-toggler";
 
 export function ModeToggle() {
 	return (
-		<AnimatedThemeToggler className="flex items-center justify-center border rounded-md h-8 w-8 text-sm shadow" />
+		<AnimatedThemeToggler className="flex items-center justify-center border rounded-md h-8 w-8" />
 	);
 }
